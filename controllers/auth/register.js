740f7141bb63eef0d646b2faf36755151a23ceb5
Taras-Veremchuk <taras.veremm@gmail.com>
2023-08-03 10:50:11 +0200

    added verify routes

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,7 +1,8 @@
 const { User } = require("../../models/user");
-const { ctrlWrapper, HttpError } = require("../../helpers");
+const { ctrlWrapper, HttpError, sendEmail } = require("../../helpers");
 const bcrypt = require("bcryptjs");
 const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
 
 const register = async (req, res, next) => {
 	const { email, password } = req.body;
@@ -13,12 +14,22 @@ const register = async (req, res, next) => {
 
 	const hashPassword = await bcrypt.hash(password, 10);
 	const avatarURL = gravatar.url(email);
+	const verificationToken = nanoid();
 
 	const newUser = await User.create({
 		...req.body,
 		password: hashPassword,
 		avatarURL,
+		verificationToken,
 	});
+	const verifyEmail = {
+		to: email,
+		subject: "verify email",
+		html: `<h3>Thank you for registering!</h3><p>Please click on
+    <a href="http://localhost:${process.env.PORT}/api/auth/verify
+    /${verificationToken}">this link</a></p>`,
+	};
+	await sendEmail(verifyEmail);
 
 	res.status(201).json({
 		user: {
